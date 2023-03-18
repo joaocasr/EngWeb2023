98e685b11af648e202d2072f40ceef1c0c134391
joaocasr <castrojoao839@gmail.com>
2023-03-18 14:32:28 +0000

    tpc5 check

--- TPC5/TPC/routes/index.js
+++ TPC5/TPC/routes/index.js
@@ -17,6 +17,7 @@ router.get('/tasks/:idTask', function(req, res, next) {
   var data = new Date().toISOString().substring(0, 16)
   Tasks.getTasks().then(tarefas=>{
     Tasks.getTask(req.params.idTask).then(task=>{
+      n = tarefas.length
       res.render('index', { list: tarefas,tarefa:task, d: data, size: n });
       console.log("entrou no rendering do index")
     }).catch(error=>{
@@ -33,6 +34,7 @@ router.post('/add', function(req, res, next) {
   var data = new Date().toISOString().substring(0, 16)
   Tasks.addTask(req.body).then(task =>{
     Tasks.getTasks().then(tarefas=>{
+      n = tarefas.length
       res.render('index', { list: tarefas,tarefa:null, d: data, size: n });
       console.log("entrou no rendering do index")
     }).catch(error=>{
@@ -49,6 +51,7 @@ router.post('/edit', function(req, res, next) {
   var data = new Date().toISOString().substring(0, 16)
   Tasks.updateTask(req.body).then(task =>{
     Tasks.getTasks().then(tarefas=>{
+      n = tarefas.length
       res.render('index', { list: tarefas,tarefa:null, d: data, size: n });
       console.log("entrou no rendering do index")
     }).catch(error=>{
